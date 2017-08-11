3b942c02c4c52508d808bd886eeba042c3fd07a7
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2017-08-11 09:14:44 -0500

    Cart: Remove state when creating cart from create action

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
     before_action :authenticate_user_from_token!
 
     def create
-      @cart = Cart.create(user: current_user, state: :collecting)
+      @cart = Cart.create(user: current_user)
       if @cart.process(params)
         render json: @cart, status: :ok
       else
